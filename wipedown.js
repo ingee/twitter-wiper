434bf8d9075060e88b82bcc9d4b023afbb48aa64
Kim,InGee <ingee.kim@gmail.com>
2022-11-25 17:43:57 +0900

    Initial Commit

---
+++ wipedown.js
@@ -0,0 +1,9 @@
+console.log('TWITTER-WIPER: loaded: ver 2022-11-24 16:40')
+window.onload = () => {
+  obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
+  console.log('TWITTER-WIPER: onload() executed: check document.getElementsByClassName("r-dkhcqf")')
+}
+window.onfocus = () => {
+  obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
+  console.log('TWITTER-WIPER: onfocus() executed: check document.getElementsByClassName("r-dkhcqf")')
+}
