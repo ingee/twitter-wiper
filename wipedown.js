e504ea502da86c6cfaa0ba805b5aa1656b282b70
InGee Kim <ingee.kim@gmail.com>
2023-05-05 06:11:05 +0900

    Add window.onpopstate event-handler

--- wipedown.js
+++ wipedown.js
@@ -1,4 +1,4 @@
-console.log('TWITTER-WIPER: loaded: ver 2022-11-24 16:40')
+console.log('TWITTER-WIPER: loaded: ver 2023-05-05 05:55 GMT+0900')
 window.onload = () => {
   obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
   console.log('TWITTER-WIPER: onload() executed: check document.getElementsByClassName("r-dkhcqf")')
@@ -6,4 +6,8 @@ window.onload = () => {
 window.onfocus = () => {
   obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
   console.log('TWITTER-WIPER: onfocus() executed: check document.getElementsByClassName("r-dkhcqf")')
+}
+window.onpopstate = () => {
+  obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
+  console.log('TWITTER-WIPER: onpopstate() executed: check document.getElementsByClassName("r-dkhcqf")')
 }
