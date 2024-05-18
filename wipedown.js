e74c1bacada0d165adf051d483c65f14b974f76c
InGee Kim <ingee.kim@gmail.com>
2024-05-18 16:45:27 +0900

    Change class-name to align with twitter.com's renewal

--- wipedown.js
+++ wipedown.js
@@ -1,13 +1,15 @@
-console.log('TWITTER-WIPER: loaded: ver 2023-05-05 05:55 GMT+0900')
+console.log('TWITTER-WIPER: loaded: ver 2024-05-18 16:25 GMT+0900')
+const className = 'r-l5o3uw'
+
 window.onload = () => {
-  obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
-  console.log('TWITTER-WIPER: onload() executed: check document.getElementsByClassName("r-dkhcqf")')
+  obj = document.getElementsByClassName(className); for (o of obj) { o.style.opacity = 0.1 }
+  console.log(`TWITTER-WIPER: onload() executed: check document.getElementsByClassName("${className}")`)
 }
 window.onfocus = () => {
-  obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
-  console.log('TWITTER-WIPER: onfocus() executed: check document.getElementsByClassName("r-dkhcqf")')
+  obj = document.getElementsByClassName(className); for (o of obj) { o.style.opacity = 0.1 }
+  console.log(`TWITTER-WIPER: onfocus() executed: check document.getElementsByClassName("${className}")`)
 }
 window.onpopstate = () => {
-  obj = document.getElementsByClassName('r-dkhcqf'); for (o of obj) { o.style.opacity = 0.1 }
-  console.log('TWITTER-WIPER: onpopstate() executed: check document.getElementsByClassName("r-dkhcqf")')
+  obj = document.getElementsByClassName(className); for (o of obj) { o.style.opacity = 0.1 }
+  console.log(`TWITTER-WIPER: onpopstate() executed: check document.getElementsByClassName("${className}")`)
 }
